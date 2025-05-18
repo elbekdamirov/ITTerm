7880c0cb10405b7ddb80f8eddea97fa50f9cba5d
elbekdamirov <elbekmain@gmail.com>
2025-05-18 09:27:38 +0500

    logger

--- middlewares/errors/error-handling.middleware.js
+++ middlewares/errors/error-handling.middleware.js
@@ -1,8 +1,9 @@
 const ApiError = require("../../error/ApiError");
+const logger = require("../../services/logger.service");
 
 module.exports = function (err, req, res, next) {
   console.log(err);
-
+  logger.error(err);
   if (err instanceof ApiError) {
     return res.status(err.status).send({ message: err.message });
   }
---
+++ services/logger.service.js
@@ -0,0 +1,38 @@
+const winston = require("winston");
+require("winston-mongodb");
+const config = require("config");
+
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, printf, prettyPrint, json, colorize } =
+  format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    // colorize({ all: true }), // consolega oddiy formatda chiqarishga
+    label({ label: "ITTERM" }),
+    timestamp(),
+    myFormat
+    // json(),
+    // prettyPrint(),
+  ),
+  transports: [
+    new transports.Console({ level: "silly" }),
+    new transports.File({ filename: "logs/error.log", level: "error" }),
+    new transports.File({ filename: "logs/combine.log", level: "info" }),
+    new transports.MongoDB({
+      db: config.get("dbUri"),
+      collection: "log",
+    }),
+  ],
+  exceptionHandlers: [new transports.File({ filename: "logs/exceptions.log" })],
+  rejectionHandlers: [new transports.File({ filename: "logs/rejections.log" })],
+});
+
+logger.exitOnError = false;
+logger.rejections;
+
+module.exports = logger;
